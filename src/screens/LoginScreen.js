ebc124cd9d6366e7586d24046aa48eb1e3299463
KiriZhu <kiribaty88@gmail.com>
2020-12-01 14:09:21 +0100

    initial commit: Added material UI dependency, created simple LoginScreen and WelcomeScreen

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import { Divider } from '@material-ui/core';
+import '../styles/Login.css';
+
+function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (email !== 'Kiribaty' && password !== '123456') {
+      setErrorMsg('You have entered the wrong credentials');
+    }
+  };
+  return (
+    <div className='centered'>
+      <div style={{ margin: 10 }} />
+      {errorMsg ? <p>{errorMsg}</p> : null}
+      <div style={{ margin: 10 }} />
+      <TextField
+        required
+        id='standard-required'
+        label='Email'
+        defaultValue='your@email.io'
+        autoCapitalize='none'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <div style={{ margin: 10 }} />
+      <TextField
+        required
+        id='standard-required'
+        label='Password'
+        defaultValue='********'
+        autoCapitalize='none'
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <div style={{ margin: 10 }} />
+      <Button variant='contained' onClick={(e) => onSubmit(e)}>
+        Sign in
+      </Button>
+    </div>
+  );
+}
+
+export default LoginScreen;
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WelcomeScreen() {
+  return (
+    <div>
+      <p>This is the welcomeScreen</p>
+    </div>
+  );
+}
+
+export default WelcomeScreen;
